fdbe8d8ea880c22655dc7f03f808c179aba0f8cd
Juhana Kivelä <juhana.kivela@tuni.fi>
2025-01-08 12:04:47 +0200

    Add correct username + ps for auth

--- Service1/springboot/src/main/java/com/example/springboot/SecurityConfiguration.java
+++ Service1/springboot/src/main/java/com/example/springboot/SecurityConfiguration.java
@@ -4,6 +4,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.Customizer;
 
@@ -21,6 +26,20 @@ public class SecurityConfiguration {
             )
             .httpBasic(Customizer.withDefaults());
         return http.build();
+        }
+    
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
+        UserDetails user = User.withUsername("adminUser")
+            .password(passwordEncoder.encode("$apr1$w7ah8scx$mg2Jg0L4sAEAokKdf2lgm."))
+            .roles("ADMIN")
+            .build();
+        return new InMemoryUserDetailsManager(user);
     }
 }
 
