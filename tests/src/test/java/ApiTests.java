5cfcc72a72c5b974276b3c1018bb6823674310df
Juhana Kivelä <juhana.kivela@tuni.fi>
2025-01-08 12:43:27 +0200

    Iterate ApiTests by putting them in specific order

--- tests/src/test/java/ApiTests.java
+++ tests/src/test/java/ApiTests.java
@@ -1,5 +1,6 @@
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
@@ -7,13 +8,19 @@ import static org.hamcrest.Matchers.*;
 
 public class ApiTests {
 
+    private static String userName;
+    private static String password;
+
     @BeforeAll
     public static void setUp() {
         // URL for the testing API in port 8197
         RestAssured.baseURI = "http://localhost:8197";
+        userName = "adminUser";
+        password = "$apr1$w7ah8scx$mg2Jg0L4sAEAokKdf2lgm.";
     }
 
     @Test
+    @Order(1)
     public void testGetStateEndpoint() {
         given()
             .when()
@@ -21,10 +28,11 @@ public class ApiTests {
             .then()
             .statusCode(200)
             .contentType("text/plain")
-            .body(equalTo("INIT"));
+            .body(equalTo("RUNNING"));
     }
 
     @Test
+    @Order(2)
     public void testPutStateWithoutAuthorization() {
         // Try to put to RUNNING, but it should respond with 401 Unauthorized
         given()
@@ -36,6 +44,22 @@ public class ApiTests {
     }
 
     @Test
+    @Order(3)
+    public void testPutStatementWithAuthorization() {
+        // Put to RUNNING, should respond with 200 OK
+        given()
+            .auth().preemptive().basic(userName, password)
+            .contentType("text/plain")
+            .when()
+            .put("/state?state=RUNNING")
+            .then()
+            .statusCode(200)
+            .contentType("text/plain")
+            .body(equalTo("RUNNING"));
+    }
+
+    @Test
+    @Order(4)
     public void testRunLogEndpoint() {
         given()
             .when()
@@ -43,6 +67,6 @@ public class ApiTests {
             .then()
             .statusCode(200)
             .contentType("text/plain")
-            .body(containsString("[]"));
+            .body(containsString("INIT -> RUNNING"));
     }
 }
